0368aca31c91b27e31685b51bea698cf3d4a32c7
Dan Osborne <djosborne10@gmail.com>
2014-02-28 16:10:01 -0500

    Initial commit

---
+++ arduino-simulator/arduino-simulator.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : arduino-simulator.cpp
+// Author      : Enabling Entrepreneurs
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello Engineers!");
+	return EXIT_SUCCESS;
+}
