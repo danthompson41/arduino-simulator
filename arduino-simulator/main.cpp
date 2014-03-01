46d004babc7b6bc1cdfca4d1d15af5abd29abb79
Dan Osborne <djosborne10@gmail.com>
2014-03-01 17:13:45 -0500

    Added serial drivers and wrote test write function

--- arduino-simulator/main.cpp
+++ arduino-simulator/main.cpp
@@ -4,11 +4,22 @@
  *  Created on: Feb 28, 2014
  *      Author: Dan
  */
+#include <iostream>
 #include <arduino-simulator.h>
+#include <serial.h>
 
 int main (void)
 {
-	HelloWorld();
+	CSerial serial;
+	if (!serial.Open(7,9600))
+	{
+		std::cout << "ERROR: Could not open serial";
+		return 0;
+	}
 
-	return 1;
+	char szMessage[] = "This is test data";
+
+	int nBytesSent = serial.SendData(szMessage, strlen(szMessage));
+
+
 }
