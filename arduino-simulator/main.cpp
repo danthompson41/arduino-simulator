101fd31c414feadff4d27255dcf81955747f00bc
Dan Osborne <djosborne10@gmail.com>
2014-03-01 19:28:05 -0500

    added command line parsing

--- arduino-simulator/main.cpp
+++ arduino-simulator/main.cpp
@@ -5,11 +5,45 @@
  *      Author: Dan
  */
 #include <iostream>
+#include <fstream>
 #include <arduino-simulator.h>
 #include <serial.h>
 
-int main (void)
+int main (int argc, char* argv[])
 {
+	// welcome
+	std::cout << "\n\n*** Arduino Simulator ***\n";
+
+	// command line args initial check
+	if (argc <= 2)
+	{
+		std::cout << "\n* FORMAT: arduino-simulator.exe [filename.txt] [COMX]";
+		return 1;
+	}
+
+	// Get Comport Number
+	int iComport;
+	if (sscanf (argv[2], "%i", &iComport) != 1)
+	{
+		std::cout << "ERROR: Comport must be an integer";
+	}
+
+	// Get Filename
+	char filename[256];
+	strcpy(filename, argv[1]);
+
+	std::cout << "  COMPort: COM" << iComport << std::endl;
+	std::cout << "SleepFile: " << filename << std::endl;
+
+	return 1;
+
+
+
+
+
+
+
+
 	CSerial serial;
 	if (!serial.Open(7,9600))
 	{
@@ -21,5 +55,10 @@ int main (void)
 
 	int nBytesSent = serial.SendData(szMessage, strlen(szMessage));
 
+	if (nBytesSent <= 0)
+	{
+		std::cout << "ERROR: Could not write data";
+	}
 
+	return 1;
 }
